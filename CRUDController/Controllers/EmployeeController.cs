238875a9e8520a3f8115ee916138adec779d9d07
mahfuz khan <mahfuzpust@gmail.com>
2023-02-16 00:28:28 +0600

    Add project files.

---
+++ CRUDController/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using CRUDController.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDController.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ CRUDController/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDController.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDController.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
